fdceeefc92d2e22e8a0bf82d2d1d45580dd48439
Igor Oliveira <igorfoliveira@gmail.com>
2020-11-02 17:39:06 -0300

    conectando ao MongoDB e ajuste no controller

--- src/main/java/com/ioliveira/controllers/UserController.java
+++ src/main/java/com/ioliveira/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.ioliveira.entities.User;
 import com.ioliveira.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping
     public ResponseEntity<List<User>> findAll() {
         return ResponseEntity.ok(userService.findAll());
     }
