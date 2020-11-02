a9645dd82454f90a0af26b43a0c996c398859621
Igor Oliveira <igorfoliveira@gmail.com>
2020-11-02 20:53:28 -0300

    Instanciaçcao do DB

--- src/main/java/com/ioliveira/SpringBootMongodbApplication.java
+++ src/main/java/com/ioliveira/SpringBootMongodbApplication.java
@@ -1,13 +1,34 @@
 package com.ioliveira;
 
+import com.ioliveira.entities.User;
+import com.ioliveira.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringBootMongodbApplication {
+public class SpringBootMongodbApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMongodbApplication.class, args);
-	}
+    @Autowired
+    private UserRepository userRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongodbApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        final User u1 = User.builder().name("Odielte Martins").email("odielte@gmail.com").build();
+        final User u2 = User.builder().name("Aline Lima").email("aline@gmail.com").build();
+        final User u3 = User.builder().name("Igor Oliveira").email("igor@gmail.com").build();
+
+        userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
+    }
 }
